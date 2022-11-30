dc770b4779d1643c8859fc85bffe30e9b684b5f8
18320007 Michelle Angelina <98033159+michelleangelinaa@users.noreply.github.com>
2022-11-30 22:50:49 +0700

    Add files via upload

--- inputambilobat.php
+++ inputambilobat.php
@@ -36,5 +36,25 @@ obat_3, jumlah_obat_3, satuan_obat_3, obat_4, jumlah_obat_4, satuan_obat_4, tang
 VALUES ('".$rekam_medis."', '".$nama_lengkap."', '".$nik."', '".$tanggal_lahir."', '".$bpjs."', '".$poli_dituju."', '".$dokter_dituju."', '".$tanggal_pemeriksaan."', '".$obat_1."', '".$jumlah_obat_1."', '".$satuan_obat_1."', '".$obat_2."', '".$jumlah_obat_2."', '".$satuan_obat_2."',  
 '".$obat_3."', '".$jumlah_obat_3."', '".$satuan_obat_3."', '".$obat_4."', '".$jumlah_obat_4."', '".$satuan_obat_4."', '".$tanggal_ambil_obat."', '".$waktu_ambil_obat."')");
 
+$updateobat1 = mysqli_query($koneksiDB, "SELECT stok FROM persediaanobat WHERE nama_obat='$obat_1'");
+$row = $updateobat1 -> fetch_assoc();
+$sisastok1 = ((int) $row["stok"]) - ((int) $jumlah_obat_1);
+$sql1 = mysqli_query($koneksiDB, "UPDATE persediaanobat SET stok='$sisastok1' WHERE nama_obat='$obat_1'");
+
+$updateobat2 = mysqli_query($koneksiDB, "SELECT stok FROM persediaanobat WHERE nama_obat='$obat_2'");
+$row = $updateobat2 -> fetch_assoc();
+$sisastok2 = ((int) $row["stok"]) - ((int) $jumlah_obat_2);
+$sql2 = mysqli_query($koneksiDB, "UPDATE persediaanobat SET stok='$sisastok2' WHERE nama_obat='$obat_2'");
+
+$updateobat3 = mysqli_query($koneksiDB, "SELECT stok FROM persediaanobat WHERE nama_obat='$obat_3'");
+$row = $updateobat3 -> fetch_assoc();
+$sisastok3 = ((int) $row["stok"]) - ((int) $jumlah_obat_3);
+$sql3 = mysqli_query($koneksiDB, "UPDATE persediaanobat SET stok='$sisastok3' WHERE nama_obat='$obat_3'");
+
+$updateobat4 = mysqli_query($koneksiDB, "SELECT stok FROM persediaanobat WHERE nama_obat='$obat_4'");
+$row = $updateobat4 -> fetch_assoc();
+$sisastok4 = ((int) $row["stok"]) - ((int) $jumlah_obat_4);
+$sql4 = mysqli_query($koneksiDB, "UPDATE persediaanobat SET stok='$sisastok4' WHERE nama_obat='$obat_4'");
+
 echo "Pengambilan Obat telah Dilakukan";
- ?>
+?>
